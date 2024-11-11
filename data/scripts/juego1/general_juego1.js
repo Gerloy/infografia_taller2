15d38cda5ea0ba6d82fc46a4eaddc43ab35e913c
Gerloy <gersilro@hotmail.com>
2024-11-11 15:18:20 -0300

    prueba

---
+++ data/scripts/juego1/general_juego1.js
@@ -0,0 +1,13 @@
+function Update(){
+
+}
+
+function Click(){}
+
+function Dibujar(){
+    let segs = floor(actual_time/1000);
+    /*sketch.texto(segs +":"+ ,);*/
+}
+
+function settime(relleno){time = 0;}
+function setactual_time(relleno){actual_time = 15000;}
